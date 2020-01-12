f716e7208db34e55b5e5e4187aa92aba3ead7938
Anh Bui <anhbui159@gmail.com>
2020-01-12 14:47:02 -0500

    Start project

--- src/main/java/com/bfs/back-end/controller/LoginController.java
+++ src/main/java/com/bfs/back-end/controller/LoginController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
